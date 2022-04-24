146ed028e9b44d09bfaf7ec1eab7f7e612103e6f
Kush Donepudi <kushkumard@googlemail.com>
2022-04-24 19:48:04 +0100

    Register Listener and Execute the Orders

--- src/main/java/com/acme/mytrader/strategy/TradingStrategy.java
+++ src/main/java/com/acme/mytrader/strategy/TradingStrategy.java
@@ -1,10 +1,27 @@
 package com.acme.mytrader.strategy;
 
+import com.acme.mytrader.execution.ExecutionService;
+import com.acme.mytrader.model.LimitOrder;
+import com.acme.mytrader.price.PriceListenerImpl;
+import com.acme.mytrader.price.PriceSource;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
 /**
  * <pre>
  * User Story: As a trader I want to be able to monitor stock prices such
  * that when they breach a trigger level orders can be executed automatically
  * </pre>
  */
+@RequiredArgsConstructor
 public class TradingStrategy {
+
+    private final ExecutionService executionService;
+    private final PriceSource priceSource;
+
+    public void executeOrders(List<LimitOrder> limitOrderList) {
+        limitOrderList.stream().forEach(limitOrder -> priceSource.addPriceListener(PriceListenerImpl.builder()
+                .limitOrder(limitOrder).executionService(executionService).priceSource(priceSource).build()));
+    }
 }
--- src/test/java/com/acme/mytrader/strategy/TradingStrategyTest.java
+++ src/test/java/com/acme/mytrader/strategy/TradingStrategyTest.java
@@ -1,9 +1,80 @@
 package com.acme.mytrader.strategy;
 
+import com.acme.mytrader.execution.ExecutionService;
+import com.acme.mytrader.model.LimitOrder;
+import com.acme.mytrader.price.PriceListenerImpl;
+import com.acme.mytrader.price.PriceSource;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
 public class TradingStrategyTest {
+
+    @Mock
+    private ExecutionService executionService;
+
+    @Mock
+    private PriceSource priceSource;
+
+    @InjectMocks
+    private TradingStrategy tradingStrategy;
+
+
     @Test
-    public void testNothing() {
+    public void shouldExecuteOrdersSuccessfully() {
+        LimitOrder limitOrderDBK = new LimitOrder("DBK", 65.00, 100);
+        LimitOrder limitOrderIBM = new LimitOrder("IBM", 55.00, 100);
+        LimitOrder limitOrderGOOGL = new LimitOrder("GOOGL", 75.00, 100);
+        PriceListenerImpl priceListenerImplDBK = PriceListenerImpl.builder().limitOrder(limitOrderDBK)
+                .executionService(executionService).priceSource(priceSource).build();
+        PriceListenerImpl priceListenerImplIBM = PriceListenerImpl.builder().limitOrder(limitOrderIBM)
+                .executionService(executionService).priceSource(priceSource).build();
+        PriceListenerImpl priceListenerImplGOOGL = PriceListenerImpl.builder().limitOrder(limitOrderGOOGL)
+                .executionService(executionService).priceSource(priceSource).build();
+
+        tradingStrategy.executeOrders(List.of(limitOrderDBK, limitOrderIBM, limitOrderGOOGL ));
+
+        priceListenerImplDBK.priceUpdate("DBK", 60);
+        priceListenerImplIBM.priceUpdate("IBM", 50);
+        priceListenerImplGOOGL.priceUpdate("GOOGL", 70);
+
+        verify(priceSource, times(3)).addPriceListener(any(PriceListenerImpl.class));
+        verify(priceSource, times(3)).removePriceListener(any(PriceListenerImpl.class));
+        verify(executionService, times(3))
+                .buy(any(String.class),any(Double.class), any(Integer.class));
     }
+
+    @Test
+    public void shouldNotExecuteOrdersForUpdatesNotBreachingThreshold() {
+        LimitOrder limitOrderDBK = new LimitOrder("DBK", 65.00, 100);
+        LimitOrder limitOrderIBM = new LimitOrder("IBM", 55.00, 100);
+        LimitOrder limitOrderGOOGL = new LimitOrder("GOOGL", 75.00, 100);
+
+        PriceListenerImpl priceListenerImplDBK = PriceListenerImpl.builder().limitOrder(limitOrderDBK)
+                .executionService(executionService).priceSource(priceSource).build();
+        PriceListenerImpl priceListenerImplIBM = PriceListenerImpl.builder().limitOrder(limitOrderIBM)
+                .executionService(executionService).priceSource(priceSource).build();
+        PriceListenerImpl priceListenerImplGOOGL = PriceListenerImpl.builder().limitOrder(limitOrderGOOGL)
+                .executionService(executionService).priceSource(priceSource).build();
+
+        tradingStrategy.executeOrders(List.of(limitOrderDBK, limitOrderIBM, limitOrderGOOGL ));
+
+        priceListenerImplDBK.priceUpdate("DBK", 60);
+        priceListenerImplIBM.priceUpdate("IBM", 60);
+        priceListenerImplGOOGL.priceUpdate("GOOGL", 80);
+
+        verify(priceSource, times(3)).addPriceListener(any(PriceListenerImpl.class));
+        verify(priceSource, times(1)).removePriceListener(any(PriceListenerImpl.class));
+        verify(executionService, times(1)).buy(any(String.class),any(Double.class), any(Integer.class));
+    }
+
 }
