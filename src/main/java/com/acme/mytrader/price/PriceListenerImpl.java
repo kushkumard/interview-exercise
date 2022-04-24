67508329176d0dba031edbfff24ba26f25556070
Kush Donepudi <kushkumard@googlemail.com>
2022-04-24 19:55:02 +0100

    added comments

--- src/main/java/com/acme/mytrader/price/PriceListenerImpl.java
+++ src/main/java/com/acme/mytrader/price/PriceListenerImpl.java
@@ -17,7 +17,9 @@ public class PriceListenerImpl implements PriceListener {
     private final PriceSource priceSource;
     private boolean limitOrderExecuted;
 
+
     @Override
+    //Basic implementation of the PriceListener, priceUpdate is triggered on this Listener for the security used while registering the listener
     public void priceUpdate(String security, double price) {
         if(!limitOrderExecuted && price < limitOrder.getThreshold()) {
             executionService.buy(limitOrder.getSecurity(), price, limitOrder.getLot());
